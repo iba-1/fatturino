b1d2381aa5e7f27d58241370d56479fae33387f1
Dario De Cianni <dario.decianni@trame-digitali.it>
2026-03-02 23:01:27 +0100

    feat(fattura-xml): pure-JS XML structure validator

--- packages/fattura-xml/src/index.ts
+++ packages/fattura-xml/src/index.ts
@@ -1,5 +1,6 @@
 export { buildFatturaXml } from "./builder.js";
 export { validateBusinessRules } from "./validation/business-rules.js";
+export { validateXml } from "./validation/xml-validator.js";
 export type { ValidationError } from "./validation/business-rules.js";
 export type {
   FatturaInput,
